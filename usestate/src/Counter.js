a1499f5dd320e56e52ab591b8da63ceff648c666
gilai cohen <gilaicohen123@gmail.com>
2022-11-21 21:21:55 +0200

    homework number 2

--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -7,14 +7,23 @@ function Counter() {
             return oldCount + 1
         }
       )
-        console.log(count)
-        
+        console.log(count);  
     }
+    function incr2(){
+        setCount(
+            function(oldCount2){
+                return 0
+            }
+        )
+        console.log(count)
+    }
+
   return (
     <div >
     <h1>Counter</h1>
     <p>Counter is at {count}</p>
     <button onClick={incr}>click to add 1 to Counter </button>
+    <button onClick={incr2}>click to reset Counter </button>
     </div>
   );
 }
